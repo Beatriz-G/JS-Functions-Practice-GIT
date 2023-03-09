d5afe3ea5891c731f5e61063a9ba462d33367a78
beatriz_g <beatrizgue20@gmail.com>
2023-03-09 15:22:25 -0800

    Return keywords practice

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ var addTogether = function (num1, num2) {
 
 addTogether(13, 72);
 addTogether(36, -2.88);
+
+//return keyword
+var addTogether = function (num1, num2) {
+    return num1 + num2;
+};
+
+alert(addTogether(36, -2.88));
+console.log(addTogether(13, 72));
+var lunchForTwo = addTogether(24.56, 18.99);
