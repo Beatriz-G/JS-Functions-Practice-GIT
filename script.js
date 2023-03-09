a90dd0d98ccfc9d6dc1169916d2a27bdeb86f94a
beatriz_g <beatrizgue20@gmail.com>
2023-03-09 15:15:25 -0800

    Added parameters

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ var welcome = function (name) {
     console.log('Welcome, ${name}. Have a great day!');
 }
 
+//arguments branch
 welcome("vera");
 welcome("Adrian");
 welcome("Aida");
+
+//parameters branch
+var addTogether = function (num1, num2) {
+    console.log(num1 + num2);
+};
+
+addTogether(13, 72);
+addTogether(36, -2.88);
